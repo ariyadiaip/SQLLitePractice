9b78103d601012b3d811bee3c441f8fcd68662ab
ariyadiaip <ariyadiaip@gmail.com>
2019-09-05 10:02:17 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/bdg/smkn4/pwpb/sqllitepractice/PersonBean.java
@@ -0,0 +1,20 @@
+package id.sch.bdg.smkn4.pwpb.sqllitepractice;
+
+public class PersonBean {
+    String name;
+    int age;
+    public PersonBean() {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
